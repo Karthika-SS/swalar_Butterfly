ca1bdf8ac82e72e3c2c19a0635b2aa207f7eb955
Karthika-SS <karthikasenthil2004@gmail.com>
2026-06-18 22:10:43 +0530

    number has been updated

--- frontend/src/pages/client/Home.jsx
+++ frontend/src/pages/client/Home.jsx
@@ -230,7 +230,7 @@ const styles = `
     border-color: var(--red);
     box-shadow: 0 0 0 3px rgba(192,57,43,0.1);
   }
-  .wc-cat-ring img { width: 100%; height: 100%; object-fit: cover; }
+  .wc-cat-ring img { width: 100%; height: 100%; object-fit: contain; background: white; }
 
   .wc-cat-initial {
     width: 100%; height: 100%;
